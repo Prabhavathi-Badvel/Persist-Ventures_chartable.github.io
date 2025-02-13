23adbd73df18f7c35d924724f1227cff0832df0c
Prabhavathi-Badvel <prabha.badvel@gmail.com>
2025-02-13 20:21:11 +0530

    cors

--- src/main/java/com/persist/chartable/security/CorsConfig.java
+++ src/main/java/com/persist/chartable/security/CorsConfig.java
@@ -1,16 +1,19 @@
 package com.persist.chartable.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer{
-    
-    // @Bean
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
-            }
- 
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Replace "*" with frontend URL if necessary
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
